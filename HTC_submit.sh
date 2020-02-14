1c9887c86d40e146f6b72e184966fb29293c6a2d
Aliaksei Raspiareza <rasp@mail.desy.de>
2020-02-14 11:36:52 +0100

    condor script to submit CreateDNN

---
+++ HTC_submit.sh
@@ -0,0 +1,36 @@
+#!/bin/sh 
+# $1 - sample
+# $2 - era 
+
+cat > $1_$2.submit <<EOF
++RequestRuntime=10000
+
+RequestMemory = 2000
+
+executable = $1_$2.sh
+
+transfer_executable = True
+universe            = vanilla
+getenv              = True
+Requirements        = OpSysAndVer == "SL6"
+
+output              = $1_$2.out
+error               = $1_$2.error
+log                 = $1_$2.log
+
+queue
+
+EOF
+
+cat > $1_$2.sh <<EOF1
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc630
+cd ${CMSSW_BASE}/src
+cmsenv
+cd -
+CreateDNN $1 $2 mt
+EOF1
+
+chmod u+x $1_$2.sh
+chmod u+x $1_$2.submit
+condor_submit $1_$2.submit
