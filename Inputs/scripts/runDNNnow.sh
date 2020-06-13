095880f076a3874952b5bd43ee033a1d82c63ccd
Andrea Cardini <andrea.cardini@cern.ch>
2020-06-13 12:15:21 +0200

    Updated scripts

--- Inputs/scripts/runDNNnow.sh
+++ Inputs/scripts/runDNNnow.sh
@@ -5,21 +5,21 @@ CHANNEL=$2
 
 if [ "$CHANNEL" == "mt" ]; then
     echo "mutau channel"
-    #./NohupParser.sh SingleMuon $YEAR $CHANNEL 
-    #./NohupParser.sh EmbeddedMuTau $YEAR $CHANNEL 
+    ./NohupParser.sh SingleMuon $YEAR $CHANNEL 
+    ./NohupParser.sh EmbeddedMuTau $YEAR $CHANNEL 
 else
     echo "etau channel"
-    #./NohupParser.sh SingleElectron $YEAR $CHANNEL 
-    #./NohupParser.sh EmbeddedElTau $YEAR $CHANNEL 
+    ./NohupParser.sh SingleElectron $YEAR $CHANNEL 
+    ./NohupParser.sh EmbeddedElTau $YEAR $CHANNEL 
 fi
-#./NohupParser.sh DYJets $YEAR $CHANNEL 
-#./NohupParser.sh WJets $YEAR $CHANNEL 
-#./NohupParser.sh TTbar $YEAR $CHANNEL 
-#./NohupParser.sh Diboson $YEAR $CHANNEL 
-#./NohupParser.sh EWKZ $YEAR $CHANNEL 
-#./NohupParser.sh HToWW $YEAR $CHANNEL 
-#./NohupParser.sh GluGluHToUncorrTauTau $YEAR $CHANNEL 
+./NohupParser.sh DYJets $YEAR $CHANNEL 
+./NohupParser.sh WJets $YEAR $CHANNEL 
+./NohupParser.sh TTbar $YEAR $CHANNEL 
+./NohupParser.sh Diboson $YEAR $CHANNEL 
+./NohupParser.sh EWKZ $YEAR $CHANNEL 
+./NohupParser.sh HToWW $YEAR $CHANNEL 
+./NohupParser.sh GluGluHToUncorrTauTau $YEAR $CHANNEL 
 ./NohupParser.sh VBFHToUncorrTauTau $YEAR $CHANNEL 
-#./NohupParser.sh WHToUncorrTauTau $YEAR $CHANNEL 
-#./NohupParser.sh ZHToUncorrTauTau $YEAR $CHANNEL 
+./NohupParser.sh WHToUncorrTauTau $YEAR $CHANNEL 
+./NohupParser.sh ZHToUncorrTauTau $YEAR $CHANNEL 
 
