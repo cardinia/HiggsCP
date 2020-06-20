e4607752c236193603bd1e4bcc8170d343c11c5d
Andrea Cardini <andrea.cardini@cern.ch>
2020-06-20 23:15:35 +0200

    Parser for etau channel

--- Inputs/scripts/NohupCardsParser.sh
+++ Inputs/scripts/NohupCardsParser.sh
@@ -3,14 +3,13 @@
 # $2 - category 
 # $3 - channel 
 
-
 cat > createcards$1_$2_$3.sh <<EOF1
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc6_amd64_gcc630
 cd ${CMSSW_BASE}/src
 cmsenv
 cd -
-CreateCards card_config_newDNN_$1.conf $2 $3
+CreateCards card_config_etau_$1.conf $2 $3
 EOF1
 
 chmod u+x createcards$1_$2_$3.sh
